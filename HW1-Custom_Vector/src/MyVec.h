336940357612797c320cf548da508dbaaad04c5b
Li Zhehao <richardhall@mail.ustc.edu.cn>
2021-10-01 16:26:59 +0800

    (annotation)

--- HW1-Custom_Vector/src/MyVec.h
+++ HW1-Custom_Vector/src/MyVec.h
@@ -16,14 +16,26 @@ class MyVec {
 public:
     MyVec();
     MyVec(int n); // 其实MyVec() 可以认为是MyVec(int n=0)，可以使用默认参数实现
-    MyVec(int n, const float t);
-    MyVec(const MyVec& anotherMyVec);
+    
+    // 需要实现的函数
+    // 创建一个长度为n的向量，每个元素为t（构造函数）
+    MyVec(int n, const float t); 
+    // 拷贝构造函数
+    MyVec(const MyVec& anotherMyVec); 
+    
+    // 向末尾增加一个元素x
+    void push_back(const float x); 
 
-    void push_back(const float x);
-    int insert(const int idx, const float x);
+    //在下标为idx的元素前增加元素x，返回x的下标（由于元素的移动，x下标其实就是idx)
+    int insert(const int idx, const float x); 
 
-    int erase(const int idx);
-    int erase(const int first, const int last);
+    // 删除下标为idx的函数（注意错误处理）返回后一个元素的下标（其实后一个元素会被移动到前面，下标仍然是idx)，如果后一个元素不在向量里，返回-1
+    int erase(const int idx); 
+
+    // 删除向量中[first,last)中元素，返回后一个元素的下标，如果后一个元素不在向量里，返回-1
+    int erase(const int first, const int last); 
+
+    // 清空向量中所有元素
     void clear();
 
     /*
